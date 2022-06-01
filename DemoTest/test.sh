4497a3069657c7fc5c9c823ba5c285d45430403c
Giang-Nguyenn <giangcongnguyenn@gmail.com>
2022-06-01 11:57:09 +0700

    update

--- DemoTest/test.sh
+++ DemoTest/test.sh
@@ -5,8 +5,8 @@ echo $VERSION
 echo "maven..."
 mvn clean package -DskipTests
 echo "build images"
-#sudo docker build -t springboot:$VERSION .
-docker login registry.gitlab.com
-sudo docker build -t registry.gitlab.com/g4435/aaa .
-sudo docker push registry.gitlab.com/g4435/aaa
-#sudo docker-compose up -d
+sudo docker build -t springboot:$VERSION .
+# docker login registry.gitlab.com
+# sudo docker build -t registry.gitlab.com/g4435/aaa .
+# sudo docker push registry.gitlab.com/g4435/aaa
+sudo docker-compose up -d
